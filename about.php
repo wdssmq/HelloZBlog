b1034771f445b6f0d5ff19da20b3165bf88243f5
沉冰浮水 <wdssmq@qq.com>
2021-05-06 17:58:21 +0800

    update

--- about.php
+++ about.php
@@ -1,31 +1,85 @@
-<!--
-  <div class="SubMenu">
-    <a href="main.php" title="首页"><span class="m-left m-now">首页</span></a>
-    <?php require_once "about.php"; ?>
-  </div>
--->
-
 <style type="text/css">
+  .relative {
+    position: relative;
+  }
+
+  .clearfix::after {
+    display: block;
+    content: "";
+    clear: both;
+  }
+
+  #divMain2 {
+    margin-bottom: 3rem;
+  }
+
   #divMain .SubMenu {
     height: auto;
   }
 
-  i.xnxf-tip {
+  .tab-content form,
+  .tab-content table {
+    margin-top: 0;
+  }
+
+  .tab-content .xnxf-about-btn {
+    display: none;
+  }
+
+  .xnxf-fade>*:not(.content-box) {
+    opacity: 0;
+  }
+
+  .xnxf-fade>.in {
+    opacity: 1;
+  }
+
+  i.xnxf-tip-dot {
     background-color: red;
-    border-radius: 7px;
-    height: 7px;
-    padding: 0;
+    border-radius: 50%;
     position: relative;
-    right: 8px;
     top: -3px;
+    right: -3px;
+    height: 7px;
     width: 7px;
+    margin-left: -7px;
+    padding: 0;
   }
 
-  #help {
+  #xnxf-about {
     padding-top: 1em;
+    display: none;
+    clear: both;
   }
 
-  #help a {
+  #xnxf-contrib {
+    display: none;
+  }
+
+  #xnxf-contrib.absolute {
+    position: absolute;
+    display: block;
+    background-color: #fff;
+    box-shadow: 0 0 0.1em rgb(0 0 0 / 30%);
+    left: 0;
+    top: 0;
+    right: 0;
+  }
+
+  #xnxf-contrib>* {
+    width: 960px;
+    margin: .7rem auto;
+  }
+
+  #xnxf-qr-box {
+    background-color: #DDDDDD;
+    padding: 11px 0;
+    margin-top: 20px;
+    display: flex;
+    justify-content: space-evenly;
+  }
+
+  #xnxf-about a {
     background-color: inherit;
     float: none;
   }
@@ -39,27 +93,88 @@
     visibility: visible;
   }
 </style>
-<i class="xnxf-tip" style="float: right"></i>
-<a href="javascript:;" style="float: right;" onclick="$('#help,#divMain2').slideToggle();$('.xnxf-tip').animate({opacity: 0});SetCookie('xnxf-tip', '2020-06-03', 73)" title="查看/隐藏关于"><span class="m-right">[关于]</span></a>
-<div id="help" style="display:none;clear: both">
+<a class="xnxf-about-btn" href="javascript:;" style="float: right;" title="查看/隐藏关于">
+  <i class="xnxf-tip-dot" style="float: right"></i>
+  <span class="m-right">[关 于]</span>
+</a>
+<div id="xnxf-about">
+  <p><img src="https://img.shields.io/badge/-SEO%E6%98%AF%E5%95%A5%E8%81%94%E7%9B%9F-yellowgreen" title="SEO是啥联盟" alt="SEO是啥联盟"> <a target="_blank" title="Feed-FeedsPub" href="https://feeds.pub/feed/https%3A%2F%2Fwww.wdssmq.com%2Ffeed.php"><img src="https://img.shields.io/badge/Feed-FeedsPub-brightgreen" title="Feed-FeedsPub" alt="Feed-FeedsPub"></a> <a target="_blank" title="Feed-Inoreader" href="https://www.innoreader.com/feed/https%3A%2F%2Fwww.wdssmq.com%2Ffeed.php"><img src="https://img.shields.io/badge/Feed-Inoreader-blue" title="Feed-Inoreader" alt="Feed-Inoreader"></a> <a target="_blank" title="Feed-feed.wdssmq.com" href="https://feed.wdssmq.com"><img src="https://img.shields.io/badge/Feed-feed.wdssmq.com-yellow" title="Feed-feed.wdssmq.com" alt="Feed-feed.wdssmq.com"></a> <a target="_blank" title="爱发电-@wdssmq" href="https://afdian.net/@wdssmq"><img src="https://img.shields.io/badge/%E7%88%B1%E5%8F%91%E7%94%B5-%40wdssmq-blueviolet" title="爱发电-@wdssmq" alt="爱发电-@wdssmq"></a> <a target="_blank" title="mastodon-wdssmq" href="https://wxw.moe/@wdssmq"><img src="https://img.shields.io/mastodon/follow/142218?domain=https%3A%2F%2Fwxw.moe%2F" title="mastodon-wdssmq" alt="mastodon-wdssmq"></a> <a target="_blank" title="QQ-349467624" href="https://wpa.qq.com/msgrd?v=3&uin=349467624&site=qq&menu=yes"><img src="https://img.shields.io/badge/QQ-349467624-0086F9" title="QQ-349467624" alt="QQ-349467624"></a></p>
+  <!-- -------- -->
   <p>QQ：349467624 <a target="_blank" href="http://wpa.qq.com/msgrd?v=3&amp;uin=349467624&amp;site=qq&amp;menu=yes" title="沉冰浮水的QQ"><img src="https://www.wdssmq.com/zb_users/logos/qq_button.png" alt="QQ" title="QQ"></a></p>
   <p>博客：<a target="_blank" href="https://www.wdssmq.com" title="沉冰浮水">https://www.wdssmq.com</a></p>
-  <p>Feed：<a target="_blank" href="https://feed.wdssmq.com" title="Feed">http://feed.wdssmq.com</a></p>
+  <p>Feed：<a target="_blank" href="https://feed.wdssmq.com" title="Feed">https://feed.wdssmq.com</a></p>
   <p>bilibili：<a target="_blank" href="https://space.bilibili.com/44744006" title="bilibili">https://space.bilibili.com/44744006</a></p>
   <p>知乎：<a target="_blank" href="https://www.zhihu.com/people/wdssmq" title="知乎">https://www.zhihu.com/people/wdssmq</a></p>
   <p>GitHub：<a target="_blank" href="https://github.com/wdssmq" title="GitHub">https://github.com/wdssmq</a></p>
   <p>GreasyFork：<a target="_blank" href="https://greasyfork.org/zh-CN/users/6865-wdssmq" title="GreasyFork">https://greasyfork.org/zh-CN/users/6865-wdssmq</a></p>
-  <hr>
+  <hr><!-- -------- -->
   <p>爱发电：<a target="_blank" href="https://afdian.net/@wdssmq" title="爱发电">https://afdian.net/@wdssmq</a></p>
   <p>[AD：<a title="老薛主机" target="_blank" href="https://my.laoxuehost.net/aff.php?aff=294">PHP美国空间</a> 优惠码：15off-xnxf ]</p>
   <p>[AD：<a title="主机云" target="_blank" href="https://my.hostyun.com/page.aspx?c=referral&u=8680">vps-主机云</a>]</p>
   <p>[AD：<a title="Vultr" target="_blank" href="https://www.vultr.com/?ref=7663955">vps-Vultr</a>]</p>
+  <hr><!-- -------- -->
+  <p>一款基于QueryList V4的Z-Blog采集插件：<a href="https://github.com/wdssmq/kumo-for-zblog" target="_blank" title="wdssmq/kumo-for-zblog: 一款基于QueryList V4的Z-Blog采集插件">https://github.com/wdssmq/kumo-for-zblog</a></p>
+  <p>某理念崩坏的采集插件 - Z-Blog 应用中心：<a href="https://app.zblogcn.com/?id=17788" target="_blank" title="某理念崩坏的采集插件 - Z-Blog 应用中心">https://app.zblogcn.com/?id=17788</a></p>
+  <div style="min-height: 37px;"></div>
 </div>
-<div style="clear: both"></div>
+<div id="xnxf-contrib" class="in">
+  <p>虽然暂时还没有做区免费 Or 收费的判断，假定您是免费获取到的，而这个插件对您有用，请支付合理费用。</p>
+  <p><b>点击上方「关于」可关闭此提示，137天内不再显示。</b></p>
+  <div id="xnxf-qr-box">
+    <img src="https://cdn.jsdelivr.net/gh/wdssmq/wdssmq@main/doc/qr-qq.png" alt="qr-qq" title="qr-qq" style="max-width:100%;">
+    <img src="https://cdn.jsdelivr.net/gh/wdssmq/wdssmq@main/doc/qr-wx.png" alt="qr-wx" title="qr-wx" style="max-width:100%;">
+    <img src="https://cdn.jsdelivr.net/gh/wdssmq/wdssmq@main/doc/qr-ali.png" alt="qr-ali" title="qr-ali" style="max-width:100%;">
+  </div>
+  <div style="min-height: 37px;"></div>
+</div>
+<div class="clearfix"></div>
 <script type="text/javascript">
-  if (GetCookie("xnxf-tip") == '2020-06-03') {
-    $('.xnxf-tip').css({
-      opacity: 0
+  $(function() {
+    const mz_blogtitle = '<?php echo $blogtitle; ?>';
+    const mz_cookie = GetCookie("xnxf-about-tip") || "";
+
+    let mz_ckeSet = 0;
+
+    if (mz_cookie.indexOf(mz_blogtitle) > -1 && 1 == 1) {
+      mz_ckeSet = 1;
+      fnHideTipDot();
+    } else {
+      $("#divMain2,.content-box-content").addClass("relative xnxf-fade");
+      let $elWrap;
+      if ($(".content-box-content").length > 0) {
+        $elWrap = $(".content-box-content");
+      } else {
+        $elWrap = $("#divMain2");
+      }
+      $("#xnxf-contrib").appendTo($elWrap).addClass("absolute");
+    }
+
+    function fnHideTipDot() {
+      $('.xnxf-tip-dot').css({
+        opacity: 0
+      });
+    }
+
+    function fnAboutToggle(inTab = false) {
+      $(".absolute").removeClass("absolute");
+      $(".xnxf-fade").removeClass("xnxf-fade");
+      if (inTab) {
+        $("#xnxf-about,#xnxf-contrib").slideDown();
+      } else {
+        fnHideTipDot();
+        $("#divMain2,#xnxf-about,#xnxf-contrib").slideToggle();
+      }
+      if (mz_ckeSet == 0) {
+        SetCookie('xnxf-about-tip', `${mz_cookie},${mz_blogtitle}`, 137)
+      }
+    }
+
+    $(".xnxf-about-btn").click(function() {
+      if ($(this).hasClass("tab-btn")) {
+        fnAboutToggle(1);
+      } else {
+        fnAboutToggle();
+      }
     });
-  }
+  });
 </script>
